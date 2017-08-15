6a89a095fa44881d9bdd874e9ea3b8004d22c75a
alexNeto <alex.neto3@hotmail.com>
2017-08-15 17:03:44 +0000

    initial commit

---
+++ LISTAS/src/lista_1/Main.java
@@ -0,0 +1,12 @@
+package lista_1;
+
+
+public class Main {
+
+	public static void main(String[] args) { 
+		
+		System.out.println("Escolha um Exercicio:");
+		
+	}
+
+}
---
+++ LISTAS/src/revisao/Main.java
@@ -0,0 +1,11 @@
+package revisao;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		System.out.println("revisao");
+
+	}
+
+}
