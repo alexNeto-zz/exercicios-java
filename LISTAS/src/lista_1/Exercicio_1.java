49c8d7f3752ff780701dd4ade0f352ac2558acc3
alexNeto <alex.neto3@hotmail.com>
2017-08-15 17:55:21 +0000

    terminado exercicio 3

--- LISTAS/src/lista_1/Exercicio_1.java
+++ LISTAS/src/lista_1/Exercicio_1.java
@@ -11,7 +11,7 @@ public class Exercicio_1 {
 		System.out.println("Digite outro número: ");
 		int numero2 = input.nextInt();
 
-		System.out.printf("A soma é %d\n", numero1 + numero2);
+		System.out.printf("\nA soma é %d\n", numero1 + numero2);
 		System.out.printf("A subtração é %d\n", numero1 - numero2);
 		System.out.printf("A multiplicação é %d\n", numero1 * numero2);
 		System.out.printf("A divisão é %.2f\n", (numero1 / (double)numero2));
--- LISTAS/src/lista_1/Exercicio_5.java
+++ LISTAS/src/lista_1/Exercicio_5.java
@@ -1,25 +1,15 @@
 package lista_1;
 
 public class Exercicio_5 {
-	
-	/*
-		Desenvolva um aplicativo que determinar� se um cliente de uma loja de departamentos
-		excedeu o limite de cr�dito em uma conta-corrente. Para cada cliente, os seguintes
-		fatos est�o dispon�veis: n�mero da conta, saldo no in�cio do m�s, total de todos os
-		itens cobrados desse cliente no m�s, total de cr�ditos aplicados ao cliente no m�s
-		e limite de cr�dito autorizado. O programa deve inserir todos esses fatos com ponteiros,
-		calcular o novo saldo (= saldo inicial + despesas - cr�ditos), exibir o novo saldo e
-		determinar se o novo saldo excede o limite de cr�dito do cliente. Avisar se o
-		cr�dito do cliente est� excedido ou n�o.
-	*/
-	
-	java.util.Scanner input = new java.util.Scanner(System.in);
-	
-	System.out.println("Digite um n�mero: ");
-	int numero1 = input.nextInt();
-	System.out.println("Digite outro n�mero: ");
-	int numero2 = input.nextInt();
-	
-	
-	System.out.println((numeros1 % numero2 == 0) ? "� divisivel" : "n�o � divisivel");
+
+	public static void exercicio() {
+		java.util.Scanner input = new java.util.Scanner(System.in);
+
+		System.out.println("Digite um n�mero: ");
+		int numero1 = input.nextInt();
+		System.out.println("Digite outro n�mero: ");
+		int numero2 = input.nextInt();
+
+		System.out.println((numeros1 % numero2 == 0) ? "� divisivel" : "n�o � divisivel");
+	}
 }
