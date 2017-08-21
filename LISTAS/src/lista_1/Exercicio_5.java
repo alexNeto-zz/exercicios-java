9052aa498a8a1bc660e455d33c29ea3df574ce45
alexNeto <alex.neto3@hotmail.com>
2017-08-21 19:58:42 +0000

    organizando o código

--- LISTAS/src/lista_1/Exercicio_5.java
+++ LISTAS/src/lista_1/Exercicio_5.java
@@ -5,7 +5,6 @@ public class Exercicio_5 {
 	public static void exercicio() {
 
 		Cliente[] clientes = new Cliente[10];
-		// se der tempo, mudar para um SQLite, ou não
 		int[] numeroConta = new int[10];
 		double[][] dadosConta = { // {saldoInicioMes, totalItensMes, totalCreditoMes, limite}
 				{ 1000.00, 300.00, 100.00, 2000.00 }, { 1000.00, 300.00, 100.00, 2000.00 },
@@ -27,5 +26,41 @@ public class Exercicio_5 {
 				System.out.println("Limite insuficiente para cliente nº " + clientes[i].getNumeroConta());
 			}
 		}
+	}
+}
+
+class Cliente {
+
+	private int numeroConta;
+	private double saldoInicioMes;
+	private double totalItensMes;
+	private double totalCreditoMes;
+	private double limite;
+	private double novoSaldo;
+	
+	public Cliente() {}
+	public Cliente(int numeroConta, double saldoInicioMes,
+			double totalItensMes, double totalCreditoMes, double limite) {
+		this.numeroConta = numeroConta;
+		this.saldoInicioMes = saldoInicioMes;
+		this.totalItensMes = totalItensMes;
+		this.totalCreditoMes = totalCreditoMes;
+		this.limite = limite;
+		
+	}
+	public boolean verificaLimite() {
+		if((this.saldoInicioMes + this.totalItensMes) < this.limite)
+			return true;
+		return false;	
+	}
+	public void setNovoSaldo() {
+		this.novoSaldo = saldoInicioMes + totalItensMes - totalCreditoMes;
+		System.out.println("Compra realizada com sucesso  para cliente nº " + numeroConta);
+	}
+	public double getNovoSaldo() {
+		return novoSaldo;
+	}
+	public double getNumeroConta() {
+		return numeroConta;
 	}
 }
---
+++ LISTAS/src/lista_2/Exercicio_3.java
@@ -0,0 +1,18 @@
+package lista_2;
+
+public class Exercicio_3 {
+
+	/*
+	 * Um estacionamento cobra uma taxa mínima de R$2,00 para estacionar por até
+	 * três horas. Um adicional de R$0,50 por hora não necessariamente inteira é
+	 * cobrado após as três primeiras horas. Escreva um aplicativo que calcule e
+	 * exiba as taxas de estacionamento para um cliente que estacionou. O aplicativo
+	 * deve receber o intervalo de tempo que o veículo ficou estacionado e exibir a
+	 * cobrança para o cliente.
+	 */
+
+	public static void exercicio() {
+
+	}
+
+}
