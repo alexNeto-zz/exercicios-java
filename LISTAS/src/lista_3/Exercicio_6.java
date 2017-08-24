1a7271ce01a03e3620906344db9ca831e204b8d9
alexNeto <alex.neto3@hotmail.com>
2017-08-24 18:40:32 +0000

    removendo sql, vazer sem mesmo

--- LISTAS/src/lista_3/Exercicio_6.java
+++ LISTAS/src/lista_3/Exercicio_6.java
@@ -1,27 +1,14 @@
 package lista_3;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Exercicio_6 {
 
-	/*
-	 * Uma loja o contratou para desenvolver um aplicativo para gerenciamento de
-	 * relacionamento com clientes e mineração de dados. O objetivo do é armazenar
-	 * informações sobre cada cliente e o total de gasto por cada cliente ao longo
-	 * de 12 meses. Dessa forma escreva um aplicativo que armazene informações de
-	 * clientes como: Nome, Email, Telefone e Endereço e de dinheiro gasto para cada
-	 * uns dos últimos 12 meses. O Aplicativo deve informa o montante gasto por
-	 * todos os clientes para cada mês e para o total do período de 12 meses, além
-	 * de informa qual foi o cliente que mais gastou dinheiro por mês e no período
-	 * total de 12 meses. O aplicativo também deve permitir cadastro e exclusão de
-	 * clientes e também a possibilidade editar dados de clientes como telefone ou
-	 * endereço.
-	 */
 
 	public static void exercicio() {
-
+		
 	}
-
 }
+
+
+class Cliente{}
+class Gastos{}
+class Endereco{}
